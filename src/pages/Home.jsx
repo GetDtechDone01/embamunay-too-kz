c764cdd7af1130ad540863bbbd60fbe5cd44120b
tosindarey274 <tosindarey274@gmail.com>
2026-03-12 18:12:21 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import HeroSection from '../components/home/HeroSection';
 import AboutPreview from '../components/home/AboutPreview';
 import ServicesPreview from '../components/home/ServicesPreview';
+import MarketDashboard from '../components/home/MarketDashboard';
 import GeographySection from '../components/home/GeographySection';
 import SocialResponsibility from '../components/home/SocialResponsibility';
 import CTASection from '../components/home/CTASection';
@@ -10,6 +11,7 @@ export default function Home() {
   return (
     <div>
       <HeroSection />
+      <MarketDashboard />
       <AboutPreview />
       <ServicesPreview />
       <GeographySection />
